1e64a6642fe26b5252d99b7bf52b297a6c117a8f
Jacob Acosta <jacobracosta@gmail.com>
2023-10-28 18:04:44 -0700

    Implemented beetle logic and added tests.

--- gameLogic/logic.util.js
+++ gameLogic/logic.util.js
@@ -4,4 +4,18 @@ export function getSingleHexMoveLength(moveBug, staticBug, destIndex) { //need b
     return(Math.abs(destIndex - staticBug.adjacentArray.indexOf(moveBug)))
 }
 
+export function isMoveOneHex(move) {
+    const moveLength = getSingleHexMoveLength(move.moveBug, move.destBug, move.destIndex)
+    if( moveLength == 1 || moveLength == 5) return true //equal to 5 only when moving from 0 to 5, or 5 to 0
+    else return false
+}
+
+export function checkSingleHexMove(move) {
+    let bIsMoveGood = false
+    if(move.moveBug.isAdjacentToBug(move.destBug)) {
+        bIsMoveGood = isMoveOneHex(move)
+    }
+    return bIsMoveGood
+}
+
 export default getSingleHexMoveLength;
--- test/basicMovement/beetle.test.js
+++ test/basicMovement/beetle.test.js
@@ -2,12 +2,80 @@ import { expect } from "chai"
 import { beetle } from "../../gameObjects/bugs.js"
 import Move from "../../gameObjects/move.js"
 import checkMove from "../../gameLogic/checkMove.js"
+import Board from "../../gameObjects/board.js"
 
 describe("Basic Movement Logic Tests: Beetle", function () {
 
-    it("tests beetle movement", function () {
-        expect(2).to.equal(2);
+    let testBoard;
+    let moveBeetle, staticBeetle1;
+    beforeEach(function() {
+        testBoard = new Board()
+        moveBeetle = new beetle("white",0)
+        staticBeetle1 = new beetle("black",0)
+        moveBeetle.addToAdjacent(3,0,staticBeetle1)
+
+        testBoard.addToBoard(moveBeetle)
+        testBoard.addToBoard(staticBeetle1)
+    });
+
+    afterEach(function() {
+        testBoard.clear()
+    });
+
+    it("tests beetle movement success: move on same bug", function () {
+        const move = new Move(moveBeetle,staticBeetle1,1)
+        const check = checkMove(move, testBoard)
+        expect(check).to.be.true;
+    })
+
+    it("tests beetle movement success: move on top of same bug", function () {
+        const move = new Move(moveBeetle,staticBeetle1,6)
+        const check = checkMove(move, testBoard)
+        expect(check).to.be.true;
+    })
+
+    it("tests beetle movement success: move on top of different bug", function () {
+        const staticBeetle2 = new beetle("black",0)
+        testBoard.addToBoard(staticBeetle2)
+
+        moveBeetle.addToAdjacent(2,5,staticBeetle2)
+        staticBeetle2.addToAdjacent(4,1,staticBeetle1)
+
+        const move = new Move(moveBeetle,staticBeetle2,6)
+        const check = checkMove(move, testBoard)
+        expect(check).to.be.true;
     })
 
     
+    it("tests beetle movement failure: discontinuity", function () {
+        const staticBeetle2 = new beetle("black",0)
+        testBoard.addToBoard(staticBeetle2)
+
+        moveBeetle.addToAdjacent(0,3,staticBeetle1)
+        moveBeetle.addToAdjacent(3,0,staticBeetle2)
+        const move = new Move(moveBeetle, staticBeetle2,6)
+        const check = checkMove(move,testBoard)
+        expect(check).to.be.false;
+    })
+
+    //redundant?
+    it.skip("tests beetle movement success: move on different bug", function () {
+        const staticBeetle2 = new beetle("black",0)
+        testBoard.addToBoard(staticBeetle2)
+
+        moveBeetle.addToAdjacent(2,5,staticBeetle2)
+        staticBeetle2.addToAdjacent(4,1,staticBeetle1)
+
+        const move = new Move(moveBeetle,staticBeetle2,0)
+        const check = checkMove(move, testBoard)
+        expect(check).to.be.true;
+    })
+
+    //redundant?
+    it.skip("tests beetle movement failure", function () {
+        const move = new Move(moveBeetle,staticBeetle1,2)
+        const check = checkMove(move, testBoard)
+        expect(check).to.be.false;
+    })
+
 })
