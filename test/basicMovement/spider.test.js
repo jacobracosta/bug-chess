ddad43d4b5fe5e3f5e50b093e45bd311b88a63f1
Jacob Acosta <jacobracosta@gmail.com>
2023-11-15 20:54:08 -0800

    Added a test case to check the case where a spider has 4 movement options. One option is currently broken.

--- test/basicMovement/spider.test.js
+++ test/basicMovement/spider.test.js
@@ -11,14 +11,14 @@ describe("Basic Movement Logic Tests: Spider", function () {
     beforeEach(function() {
         testBoard = new Board(7)
         moveSpider = new spider("white",[4,5])
-        staticSpider1 = new spider("black",[6,4])
+        staticSpider1 = new spider("black",[6,6])
 
         testBoard.addToBoard(moveSpider)
         testBoard.addToBoard(staticSpider1)
     });
 
     it("tests spider movement success: move on same bug", function () {
-        const move = new Move(moveSpider,[8,3])
+        const move = new Move(moveSpider,[8,7])
         const [check,message] = checkMove(move, testBoard)
         expect(check).to.be.true;
         expect(message).to.eq("Success")
@@ -28,21 +28,45 @@ describe("Basic Movement Logic Tests: Spider", function () {
         const staticSpider2 = new spider("black",[8,5])
         testBoard.addToBoard(staticSpider2)
 
-        const move = new Move(moveSpider,[10,6])
+        const move = new Move(moveSpider,[10,4])
         const [check,message] = checkMove(move, testBoard)
         expect(check).to.be.true;
         expect(message).to.eq("Success")
     })
 
     it("tests spider movement failure", function () {
-        const move = new Move(moveSpider,[4,3])
+        const move = new Move(moveSpider,[8,5])
         const [check,message] = checkMove(move, testBoard)
         expect(check).to.be.false;
         expect(message).to.eq("Move Not Legal")
     })
 
-    //add case for three options of movement
+    it("tests spider movement success: 4 options", function (){
+        const staticSpider2 = new spider("black",[8,7])
+        const staticSpider3 = new spider("black",[10,6])
+        const staticSpider4 = new spider("black",[12,5])
+        const staticSpider5 = new spider("black",[12,3])
+        const staticSpider6 = new spider("black",[12,1])
+        const staticSpider7 = new spider("black",[10,0])
+        const staticSpider8 = new spider("black",[8,1])
+        const staticSpider9 = new spider("black",[6,2])
+        testBoard.addToBoard(staticSpider2)
+        testBoard.addToBoard(staticSpider3)
+        testBoard.addToBoard(staticSpider4)
+        testBoard.addToBoard(staticSpider5)
+        testBoard.addToBoard(staticSpider6)
+        testBoard.addToBoard(staticSpider7)
+        testBoard.addToBoard(staticSpider8)
+        testBoard.addToBoard(staticSpider9)
 
-    //test for continuity
+        const move1 = new Move(moveSpider,[10,2])
+        const move2 = new Move(moveSpider,[10,4])
+        const move3 = new Move(moveSpider,[4,1]) //this is the hard case
+        const move4 = new Move(moveSpider,[8,9])
 
+        const [check,message] = checkMove(move3, testBoard)
+        expect(check).to.be.true;
+        expect(message).to.eq("Success")
+
+    })
 })
