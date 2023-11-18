80fa9512dd0b8cd02b3e731c61464c9768d291ae
Jacob Acosta <jacobracosta@gmail.com>
2023-11-18 10:01:32 -0800

    Moved some loop logic into a utils file so it can be pulled out easily.

--- gameCode/game/gameLoop.js
+++ gameCode/game/gameLoop.js
@@ -3,17 +3,10 @@ import { Player } from "../../gameCode/game/player.js"
 import Board from "../../gameCode/gameObjects/board.js"
 
 import promptSync from 'prompt-sync';
-import Move from "../gameObjects/move.js";
-import Placement from "./placement.js";
-import addBugToGame, { checkPlacement } from "./gameLogic.js";
+import {isWinConditionMet, processMovement, processPlacement } from "./gameLoop.util.js";
 const prompt = promptSync({sigint:true});
 
 let queenSurrounded = false;
-
-//create players
-//when bug is placed, add to player
-//add to board, need logic to check if it's next to a piece of it's own color or not
-//need to check that there aren't more than the allowed number of bugs during each placement
 
 const red = new Player("red", true)
 const blue = new Player("blue", false)
@@ -21,15 +14,13 @@ const board = new Board(15)
 
 while (!queenSurrounded) {
 
-  const p1Surrounded = red.isQueenSurrounded(board)
-  const p2Surrounded = blue.isQueenSurrounded(board)
-  if(p1Surrounded || p2Surrounded) {
-    if(p1Surrounded && !p2Surrounded) console.log("Game Over! Player 2 Wins!")
-    else if(p2Surrounded && !p1Surrounded) console.log("Game Over! Player 1 Wins!")
-    else console.log("Game Over! Draw.")
+  const [winConditionMet, message] = isWinConditionMet(red,blue,board)
+  if(winConditionMet) {
+    console.log(message)
+    break
   }
 
-  let userInput, currentPlayer, currentBug
+  let userInput, currentPlayer
   if(board.turn % 2 != 0) { 
     userInput = prompt("Player 1's Turn: ")
     currentPlayer = red
@@ -52,48 +43,15 @@ while (!queenSurrounded) {
   }
 
   if(verb == "move") {
-    if(!currentPlayer.queenBee){
-      console.log("Cannot move until your queen is placed.")
+    const [processSuccess, message] = processMovement(currentPlayer,bug,index,coord,board)
+    if(!processSuccess) {
+      console.log(message)
       continue
-    } else {
-      if(bug == "queenBee") currentBug = currentPlayer.queenBee
-      else if (bug == "beetle") currentBug = currentPlayer.beetles[index]
-      else if (bug == "hopper") currentBug = currentPlayer.hoppers[index]
-      else if (bug == "spider") currentBug = currentPlayer.spiders[index]
-      else if (bug == "ant") currentBug = currentPlayer.ants[index]
-      else console.log("Invalid bug. Re-input command.")
-      const move = new Move(currentBug, coord)
-      const [bIsMoveGood, failureMessage] = checkMove(move, board)
-      if(bIsMoveGood) {
-        board.updateBugCoord(move)
-        board.incrementTurn()
-        continue
-      } else {
-        console.log(failureMessage, "Re-input command.")
-        continue
-      }
     }
   } else if(verb == "place") {
-    if(currentPlayer.isFirst) {
-      if(board.turn >= 7 && !currentPlayer.queenBee){
-        console.log("You must place your queen by your 4th turn.")
-        continue
-      }
-    } else {
-      if(board.turn >= 8 && !currentPlayer.queenBee){
-        console.log("You must place your queen by your 4th turn.")
-        continue
-      }
-    }
-
-    const placement = new Placement(currentPlayer, coord, bug)
-    const [bIsPlacementGood, failureMessage] = checkPlacement(placement,board)
-    if(bIsPlacementGood) {
-      addBugToGame(placement, board)
-      board.incrementTurn()
-      continue
-    } else {
-      console.log(failureMessage, "Re-input command.")
+    const [processSuccess, message] = processPlacement(currentPlayer,bug,coord,board)
+    if(!processSuccess) {
+      console.log(message)
       continue
     }
   } else {
