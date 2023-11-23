88a38b27d6c4198ef9c0d5070415f5b8e1bf5dc3
Jacob Acosta <jacobracosta@gmail.com>
2023-11-23 09:48:02 -0800

    Added note on why test is failing.

--- test/fullGameLoop/fullGameLoop.test.js
+++ test/fullGameLoop/fullGameLoop.test.js
@@ -12,7 +12,7 @@ describe("Basic Game Loop Test: Full Game Loop", function (){
         expect(errors).to.be.an("array").that.is.empty
     })
 
-    it.skip("Tests Full Loop: Success, Beetle Traversal Test", function (){
+    it.skip("Tests Full Loop: Success, Beetle Traversal Test", function (){ //cell getting erased when beetle traversing across top
         const [gameOverMessage, errors] = gameLoopTestHandle(loopTestBeetleJson["commands"])
         console.log("errors",errors)
         expect(gameOverMessage).to.eq("Game Over! Player 1 Wins!")
